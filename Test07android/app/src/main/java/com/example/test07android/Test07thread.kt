751d20bf7611f4fcc39a2355ced8fef91b9c1d05
wisdompark <ifyouwonder90@naver.com>
2019-05-10 16:11:11 +0900

    4일차 commit

---
+++ Test07android/app/src/main/java/com/example/test07android/Test07thread.kt
@@ -0,0 +1,32 @@
+package com.example.test07android
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
+
+fun main(){
+
+    println("main start")
+    /*thread(true){
+        for(i in 90..99){
+            print("$i ")
+            Thread.sleep(500)
+        }
+    }*/
+
+    launch{
+        for(i in 90..99){
+            print("$i ")
+            Thread.sleep(500)
+        }
+    }
+    runBlocking { val job = launch(Dispatchers.Default){
+        for(i in 1..10){
+            print("$i ")
+            Thread.sleep(500)
+        }
+    } }
+    println("main end")
+}
